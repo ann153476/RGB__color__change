4ba7a14cd160d673710cae337e0791402ef1abd9
Anka <103377387+ann153476@users.noreply.github.com>
2023-04-01 23:45:07 +0300

    all

---
+++ index.js
@@ -0,0 +1,51 @@
+const spanR = document.querySelector(".red__span");
+const spanG = document.querySelector(".green__span");
+const spanB = document.querySelector(".blue__span");
+const spanT = document.querySelector(".transparent__span");
+
+const inputR = document.querySelector("#red__input");
+const inputG = document.querySelector("#green__input");
+const inputB = document.querySelector("#blue__input");
+const inputT = document.querySelector("#transparent__input");
+
+const myBox = document.querySelector(".box");
+
+const body = document.querySelector(".color__box");
+
+const codeText = document.querySelector(".code");
+
+inputR.oninput = () => {
+  let value = inputR.value;
+  spanR.textContent = Math.trunc((value * 100) / inputR.max) + "%";
+  spanR.style.left = (value * 100) / inputR.max + "%";
+};
+
+inputG.oninput = () => {
+  let value = inputG.value;
+  spanG.textContent = Math.trunc((value * 100) / inputG.max) + "%";
+  spanG.style.left = (value * 100) / inputG.max + "%";
+};
+
+inputB.oninput = () => {
+  let value = inputB.value;
+  spanB.textContent = Math.trunc((value * 100) / inputB.max) + "%";
+  spanB.style.left = (value * 100) / inputB.max + "%";
+};
+inputT.oninput = () => {
+  let value = inputT.value;
+  spanT.textContent = Math.trunc((value * 100) / inputT.max) + "%";
+  spanT.style.left = (value * 100) / inputT.max + "%";
+};
+function func() {
+  codeText.textContent = `rgba(${Number(inputR.value)},${Number(
+    inputG.value
+  )},${Number(inputB.value)},${(1 - Number(inputT.value)).toFixed(2)})`;
+
+  body.style.backgroundColor = `rgba(${Number(inputR.value)},${Number(
+    inputG.value
+  )},${Number(inputB.value)},${(1 - Number(inputT.value)).toFixed(2)})`;
+}
+
+document.addEventListener("DOMContentLoaded", func);
+
+myBox.addEventListener("input", func);
